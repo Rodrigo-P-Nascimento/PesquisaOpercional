b07d4e1031d29be2ea3622e5ff039a57f2f1578d
LuizGusttavo <luiz.gusttavo@lavid.ufpb.br>
2023-05-30 10:54:05 -0300

    criação da classe Solver e objeto resolvedor

--- main.py
+++ main.py
@@ -1,44 +1,5 @@
-print("Bem vindo, vamos começar lendo os dados de entrada!")
-try:
-    # Tentamos abrir o arquivo para leitura
-    fileOpened = True
-    arquivo = open('entrada.txt', 'r')
+from Solver import Solver
 
-#Tratamento de erros de leitura do arquivo 'entrada.txt'
-except FileNotFoundError:
-    print("O arquivo não foi encontrado.")
-    fileOpened = False
-except PermissionError:
-    print("Você não tem permissão para ler o arquivo.")
-    fileOpened = False
-except Exception as e:
-    print("Ocorreu um ERRO durante a leitura do arquivo:", str(e))
-    fileOpened = False
+resolvedor = Solver()
 
-if fileOpened == False:
-    exit(-1)#retornamos -1 como erro
-
-print("\nDados de entrada lidos com sucesso!")
-
-linha1 = arquivo.readline()#lê a primeira linha do arquivo
-
-numVar = int(linha1.split()[0])#numero de variaveis
-numRest = int(linha1.split()[1])#numero de restrinções
-
-linha2 = arquivo.readline()#lê a segunda linha do arquivo
-valores = linha2.split()
-valFunObj = [int(val) for val in valores]#adiciona os valores int na lista, esses são os valores das Função Objetivo
-
-restrincoes = {} #cria um dicionario vazio para as restrinções
-
-for i in range(1, numRest+1):#Vamos pegar os valores das N linhas relacionadas as restrinções
-    linhas = arquivo.readline()
-    ResValores = linhas.split()  
-    restrincoes[f"Restrincoes{i}"] = [int(val) for val in ResValores]
-
-print(numVar)
-print(numRest)
-print(valFunObj)
-print(restrincoes)
-
-arquivo.close() #fechamos o arquivo
+resolvedor.resolver()
